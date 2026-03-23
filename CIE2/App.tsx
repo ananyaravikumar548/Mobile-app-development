c2cee53beaa236041ceb3002e29146366755ffbe
ananyaravikumar548 <ananyaravikumar71@gmail.com>
2026-03-23 16:34:37 +0530

    Create App.tsx

---
+++ CIE2/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import WelcomeScreen from './WelcomeScreen';
+import CreateAccountScreen from './CreateAccountScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="CreateAccount" component={CreateAccountScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
